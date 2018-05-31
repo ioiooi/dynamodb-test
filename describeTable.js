661ef58d5e4765f282a4f3d582c96ffafb911a17
Eugen Mayer <eugen.mayer2@gmail.com>
2018-05-31 17:49:46 +0200

    table infos... like total number of items

---
+++ describeTable.js
@@ -0,0 +1,26 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  region: 'eu-central-1',
+  endpoint: 'http://localhost:8000'
+});
+
+const dynamodb = new AWS.DynamoDB();
+
+const params = {
+  TableName: 'Movies'
+};
+
+dynamodb.describeTable(params, (err, data) => {
+  if (err) {
+    console.error(
+      'Unable to describe table. Error JSON:',
+      JSON.stringify(err, null, 2)
+    );
+  } else {
+    console.log(
+      'DescribeTable:',
+      JSON.stringify(data, null, 2)
+    );
+  }
+});
